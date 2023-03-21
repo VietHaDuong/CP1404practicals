2593509397ea56af4ce7a04465be36659fdfe326
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-03-21 10:49:20 +0800

    Add score menu

--- prac_2/score_menu.py
+++ prac_2/score_menu.py
@@ -6,27 +6,28 @@ MENU ="""(G)et a valid score
 
 def main():
     print(MENU)
+    score = get_valid_score()
     choice = input('>>> ').lower()
     while choice != 'q':
-        option(choice)
+        if choice == 'g':
+            score = get_valid_score()
+        elif choice == 'p':
+            print(determine_grade(score))
+        elif choice == 's':
+            generating_stars(score)
+        choice = input('>>> ').lower()
     print('Goodbye!')
 
 
-def option(choice):
-    if choice == 'g':
-        score = get_score()
-    elif choice == 'p':
-        print(determine_grade(score))
-    elif choice == 's':
-        generating_stars()
-
-
-def generating_stars():
+def generating_stars(score):
     print(score * '*')
 
 
-def get_score():
+def get_valid_score():
     score = int(input('Enter score: '))
+    while score < 0 or score > 100:
+        print('Invalid score!')
+        score = int(input('Enter score: '))
     return score
 
 
@@ -39,6 +40,7 @@ def determine_grade(score):
         return "Passable"
     else:
         return "Bad"
+
 
 main()
 
