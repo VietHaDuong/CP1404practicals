f90a142ef5a1cacb3eeda63f6af7f6d1c7d5b1be
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-03-27 13:07:45 +0800

    add random number generator

---
+++ prac_3/randoms.py
@@ -0,0 +1,20 @@
+import random
+
+# What did you see on line 1?
+# A print function that print random integer in range from 5 to 20
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number that I have seen is 8 and the largest is 20
+
+# What did you see on line 2?
+# A print function that print random number in a given range that might not include the endpoint
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number that I see is 3 and the largest is 9
+# Could line 2 have produced a 4?
+# No because the step is 2 and the starting point is 3, therefore only odd number is produced
+
+# What did you see on line 3?
+# A print function that print random number in a range of something depending on it rounding
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 2.579996556258525 and the largest is 4.329172919766227
+
+print(random.randint(1, 100))
