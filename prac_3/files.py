21dbf0d1de8bf3e995c53c7ae98725bf18ec67b1
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-03-28 09:52:36 +0800

    code that open, read and write to text file

--- prac_3/files.py
+++ prac_3/files.py
@@ -1,1 +1,24 @@
+# Code ask for user's name
 name = input('Enter name: ')
+out_file = open('name.txt', 'w')
+out_file.write(name)
+out_file.close()
+
+# Code that open file and read name
+in_file = open('name.txt', 'r')
+print(f'Your name is {in_file.read().strip()}')
+
+# Code that open file and read the first 2 numbers then print the sum of that 2 numbers
+read_file = open('numbers.txt', 'r')
+first_number = int(read_file.readline())
+second_number = int(read_file.readline())
+read_file.close()
+print(first_number + second_number)
+
+# Print total sum of all numbers
+in_file = open('numbers.txt', 'r')
+total = 0
+for line in in_file:
+    total += int(line)
+in_file.close()
+print(total)
