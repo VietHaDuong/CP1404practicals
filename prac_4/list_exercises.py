5a570817feafd28ad19e5c4c35c74047558a8981
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-04-03 12:32:00 +0800

    practice exercises in list

--- prac_4/list_exercises.py
+++ prac_4/list_exercises.py
@@ -1,3 +1,4 @@
+# Basic list operations
 numbers = []
 for i in range(5):
     number = float(input('Number: '))
@@ -7,3 +8,13 @@ print(f'The last number is {numbers[-1]}')
 print(f'The smallest number is {min(numbers)}')
 print(f'The largest number is {max(numbers)}')
 print(f'The average of the numbers is {sum(numbers) / len(numbers)}')
+
+print()
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name = input('Username: ')
+if name in usernames:
+    print('Access granted')
+else:
+    print('Access denied')
