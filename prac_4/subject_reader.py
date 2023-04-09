3410be5c9c04fc3bb230eb5df0bcc4f9edace2eb
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-04-09 18:09:36 +0800

    add display function

--- prac_4/subject_reader.py
+++ prac_4/subject_reader.py
@@ -4,15 +4,14 @@ Data file -> lists program
 """
 
 FILENAME = "subject_data.txt"
-list_of_data = []
 
 def main():
     data = get_data()
-    display_detail(list_of_data)
-    print(data)
+    display_detail(data)
 
 
 def get_data():
+    list_of_data = []
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
@@ -24,9 +23,9 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         list_of_data.append(parts)
-        print(list_of_data)
         print("----------")
     input_file.close()
+    return list_of_data
 
 def display_detail(list_of_data):
     for subject in list_of_data:
