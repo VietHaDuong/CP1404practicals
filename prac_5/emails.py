17371289fdd16eb3fc1f2b270d6112037c7e4108
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-04-10 22:14:35 +0800

    complete emails exercise

--- prac_5/emails.py
+++ prac_5/emails.py
@@ -1,1 +1,13 @@
+names_emails = dict()
 email = input('Email: ')
+while email != '':
+    name = ' '.join(email.split('@')[0].split('.')).title()
+    confirmation = input(f'Is your name {name}? (Y/n) ').lower()
+    if confirmation != 'y' and confirmation != '':
+        name = input('Name: ')
+    names_emails[email] = name
+    email = input('Email: ')
+
+print()
+for email, name in names_emails.items():
+    print(f'{name} ({email})')
