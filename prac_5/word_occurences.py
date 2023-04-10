8cc910d12b1573f137ca68dae6186758b1502ead
Viet Ha Duong <vietha.duong@my.jcu.edu.au>
2023-04-10 15:05:48 +0800

    done most, haven't added sorting yet

---
+++ prac_5/word_occurences.py
@@ -0,0 +1,14 @@
+word_occurences = dict()
+text = input('Text: ')
+words = text.split()
+longest_word = len(max(words, key=len))
+for word in words:
+    if word in word_occurences:
+        word_occurences[word] += 1
+    else:
+        word_occurences[word] = 1
+
+for word, count in word_occurences.items():
+    print(f'{word:{longest_word}} : {count}')
+
+
